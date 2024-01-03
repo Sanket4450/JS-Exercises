bfe019dcb0170742335bb30d734e0a095763cdcd
Sanket4450 <www.talaviyasanket50@gmail.com>
2024-01-03 18:03:45 +0530

    reverse string program done

---
+++ 02/04.js
@@ -0,0 +1,9 @@
+// Write a JavaScript program to reverse a given string.
+
+function reverseString(str) {
+    return str.split('').reverse().join('')
+}
+
+console.log(reverseString('Sanket'))
+console.log(reverseString('ABCD'))
+console.log(reverseString('KRK'))
