c0dd500463e057c4cc27e9eb1bae8e41831f107e
Sanket4450 <www.talaviyasanket50@gmail.com>
2024-01-25 22:25:30 +0530

    reverse array program done

---
+++ 03/02.js
@@ -0,0 +1,13 @@
+// Write a JavaScript program to reverse the elements of a given array of integers.
+
+function reverseArray(arr = []) {
+    return arr.reverse()
+}
+
+let result1 = reverseArray([10, 24, 200, 8, 40, 90])
+let result2 = reverseArray([10, 20, 30, 40, 50])
+let result3 = reverseArray([25, 60, 2, 5, 32])
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
